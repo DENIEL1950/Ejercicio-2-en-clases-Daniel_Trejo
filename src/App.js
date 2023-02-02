7b3b35d328c3e15212bb85be5541aeb804a93c99
Fresco <Cevallosnancy@homail.com>
2023-02-02 13:00:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Boton from './Components/Boton';
+import { useState } from 'react';
 
 function App() {
+  const[valor, setValor] = useState("");
+  const hacerClic=(texto) => {
+  // console.log(texto);
+  setValor(valor + texto);
+  }
+
+  //calcular con signo igual
+  const calcularClic = () => {
+    console.log("La operacion a calcular es: ", valor)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Calculadora
+     <div>
+      <input type="text" value={valor} readOnly/>
+     </div>
+     <div>
+      <Boton hacerClic={hacerClic}>1</Boton>
+      <Boton hacerClic={hacerClic}>2</Boton>
+      <Boton hacerClic={hacerClic}>3</Boton>
+      <Boton hacerClic={hacerClic}>+</Boton>
+     </div>
+     <div>
+      <Boton hacerClic={hacerClic}>4</Boton>
+      <Boton hacerClic={hacerClic}>5</Boton>
+      <Boton hacerClic={hacerClic}>6</Boton>
+      <Boton hacerClic={hacerClic}>-</Boton>
+     </div>
+     <div>
+      <Boton hacerClic={hacerClic}>7</Boton>
+      <Boton hacerClic={hacerClic}>8</Boton>
+      <Boton hacerClic={hacerClic}>9</Boton>
+      <Boton hacerClic={hacerClic}>*</Boton>
+     </div>
+     <div>
+      <Boton hacerClic={hacerClic}>.</Boton>
+      <Boton hacerClic={hacerClic}>0</Boton>
+      <Boton hacerClic={calcularClic}>=</Boton>
+      <Boton hacerClic={hacerClic}>/</Boton>
+     </div>
+     <div>
+      <button onClick={() => setValor("")}>Clear</button>
+     </div>
     </div>
   );
 }
---
+++ src/Components/Boton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Boton(props) {
+
+return (
+    <button onClick = { () =>props.hacerClic(props.children)}>{props.children}</button>
+  )
+}
